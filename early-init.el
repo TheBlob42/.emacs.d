4cc493868db01979fbad3fd1dd526a3b475b0be8
TheBlob42 <hessenmobbel@web.de>
2020-07-26 22:22:32 +0200

    remove font settings from early init

--- early-init.el
+++ early-init.el
@@ -20,10 +20,6 @@
 ;; larger than the system default.
 (setq frame-inhibit-implied-resize t)
 
-;; Set default font
-(set-face-attribute 'default nil :font "Source Code Pro Medium")
-(set-fontset-font t 'latin "Noto Sans")
-
 ;; Ignore X resources; its settings would be redundant with the other settings
 ;; in this file and can conflict with later config (particularly where the
 ;; cursor color is concerned).
