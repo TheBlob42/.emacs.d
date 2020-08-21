f96a7f649652dc91042dbc50d574c7b41a8233fc
TheBlob42 <hessenmobbel@web.de>
2020-08-21 20:33:48 +0200

    update all lsp-related packages

---
+++ elpa/lsp-dart-20200814.1405/lsp-dart-pkg.el
@@ -0,0 +1,15 @@
+(define-package "lsp-dart" "20200814.1405" "Dart support lsp-mode"
+  '((emacs "26.1")
+    (lsp-treemacs "0.2")
+    (lsp-mode "7.0")
+    (dap-mode "0.5")
+    (f "0.20.0")
+    (dash "2.14.1")
+    (pkg-info "0.4")
+    (dart-mode "1.0.5"))
+  :commit "437c548d411c9e166b7c658fd45294775235fd5f" :keywords
+  '("languages" "extensions")
+  :url "https://emacs-lsp.github.io/lsp-dart")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
