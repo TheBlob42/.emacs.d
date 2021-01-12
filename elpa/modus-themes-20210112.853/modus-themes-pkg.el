fb204d4256da7f003685f3248a9b7448d4a03096
TheBlob42 <hessenmobbel@web.de>
2021-01-12 20:25:52 +0100

    update packages
    - all-the-icons
    - avy
    - dash
    - deft
    - dired-sidebar
    - exec-path-from-shell
    - hl-todo
    - hydra
    - iflipb
    - json-snatcher
    - modus-themes
    - posframe
    - spaceline
    - sudo-edit
    - use-package
    - which-key

---
+++ elpa/modus-themes-20210112.853/modus-themes-pkg.el
@@ -0,0 +1,12 @@
+(define-package "modus-themes" "20210112.853" "Highly accessible themes (WCAG AAA)"
+  '((emacs "26.1"))
+  :commit "fba8fb08544dd4a19fcc5c0b2ca50490bf4b10e9" :authors
+  '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainer
+  '("Protesilaos Stavrou" . "info@protesilaos.com")
+  :keywords
+  '("faces" "theme" "accessibility")
+  :url "https://gitlab.com/protesilaos/modus-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
