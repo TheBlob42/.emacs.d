00b7f2c2a0415f3863cc94f6c941b842fe4c3a52
TheBlob42 <hessenmobbel@web.de>
2021-01-16 16:20:54 +0100

    update packages
    - all-the-icons
    - dash
    - evil-collection
    - flycheck
    - flyspell-correct
    - git-commit
    - ht
    - iedit
    - modus-themes
    - posframe
    - treemacs
    - treemacs-evil

---
+++ elpa/modus-themes-20210116.624/modus-themes-pkg.el
@@ -0,0 +1,12 @@
+(define-package "modus-themes" "20210116.624" "Highly accessible themes (WCAG AAA)"
+  '((emacs "26.1"))
+  :commit "7ac62ded8506373098682382c8477cc1cfab7c69" :authors
+  '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainer
+  '("Protesilaos Stavrou" . "info@protesilaos.com")
+  :keywords
+  '("faces" "theme" "accessibility")
+  :url "https://gitlab.com/protesilaos/modus-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/treemacs-evil-20210107.1251/treemacs-evil-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/tobi/.emacs.d/elpa/treemacs-evil-20210107.1251/treemacs-evil.el  -*- no-byte-compile: t -*-
+(define-package "treemacs-evil" "20210107.1251" "Evil mode integration for treemacs" '((emacs "26.1") (evil "1.2.12") (treemacs "0.0")) :commit "1670819786f1b82cc5c542aa7eabcf29a53074a4" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
